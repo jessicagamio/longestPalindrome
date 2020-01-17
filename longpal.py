11477654f39f83a6feb6f60a9330d116fd746dfa
jessicagamio <jessica.gamio@gmail.com>
2020-01-16 16:59:19 -0800

    debugging code

--- longpal.py
+++ longpal.py
@@ -1,39 +1,48 @@
+import unittest
 
-def longestPalindrome(self, s: str) -> str:
+def isPalindrome(s):
+    """return True if palindrome""" 
+    # first check if the whole word is a palindrome
+    if len(s) % 2 == 0: # if even number
+        midpt = len(s)//2
+
+    elif len(s) % 2 != 0: # if odd number
+        midpt = len(s)//2 - 1
+
+    if s[:midpt] == s[midpt:]:
+        return True
+    else:
+        return False
+
+
+def longestPalindrome(s):
   
-    def isPalindrome(string):
-        """return True if palindrome""" 
-        # first check if the whole word is a palindrome
-        if len(s) % 2 == 0: # if even number
-            midpt = len(s)//2
-
-        elif len(s) % 2 != 0: # if odd number
-            midpt = len(s)//2 - 1
-
-        if s[:midpt] == s[midpt:]:
-            return True
-        else:
-            return False
-
-    
-
-    
     # if word is NOT a palindrome traverse string
-    sub = ''
+    substring = ''
     seen = '' # store letters that have already been reviewed
     longest = '' # holds the longest palindrome
     
     # traverse ltr in string
-        
-        # if seenis empty
-            # add ltr to sub 
-            # add ltr to seen
-        # else
-            # iterated seen_ltr through seen[::-1]
-                # sub equals ltr plus seen_ltr
-                # if sub isPalindrome:
+    for ltr in s:  
+        if seen == '': # if seenis empty
+            seen = ltr# add ltr to seen
+        else: # else
+            for seen_ltr in seen[::-1]:# iterated seen_ltr through seen[::-1]
+                substring = ltr + seen_ltr # sub equals ltr plus seen_ltr
+                
+                if isPalindrome(substring):# if sub isPalindrome:
                     # compare lenof longest with len of sub
-                    # if len(sub) is longer than len(longest)
-                        # make longest = sub
+                    if len(substring) > len(longest):# if len(sub) is longer than len(longest)
+                        longest = substring# make longest = sub
 
-        # return longest
+    # return longest
+    return longest
+
+
+class TestFunction(unittest.TestCase):
+    def testLongestPalindrome(self):
+        self.assertEqual(longestPalindrome('babad'),'bab')
+
+
+if __name__=="__main__":
+    unittest.main()
